b7af7028f397df5217a7867acedb907618338ac0
Patrick Matherly <patrickmatherly@patricks-mbp.somers.ibm.com>
2015-06-03 10:02:05 -0400

    Initial Commit

---
+++ Pokedex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pokedex
+//
+//  Created by Patrick Matherly on 6/3/15.
+//  Copyright (c) 2015 Patrick Matherly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pokedex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pokedex
+//
+//  Created by Patrick Matherly on 6/3/15.
+//  Copyright (c) 2015 Patrick Matherly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
